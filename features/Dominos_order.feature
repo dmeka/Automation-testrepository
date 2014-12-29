b8116a5c8f11666b1db7c9bbea38fc4152f3561a
Deepika Meka <dmeka@gannett.com>
2014-12-29 10:09:20 -0500

    Added more test scenarios

--- features/Dominos_order.feature
+++ features/Dominos_order.feature
@@ -4,6 +4,13 @@ Feature: Placing an online order
 
   @placeorder
   Scenario: Ordering pizza on the dominos website
+
     When I visit the Dominos Home page
     Then I click on the order online link
-    Then I fill the details to place the online order
+    Then I fill the details 
+    Then I click on the Continue button
+    Then I check the total on my order
+    Then I add a coupon code
+    Then I add extras
+    Then i check my total again
+
--- features/step_definitions/dominos_order.rb
+++ features/step_definitions/dominos_order.rb
@@ -4,24 +4,53 @@ end
 
 
 Then(/^I click on the order online link$/) do
-  click_link('Order Online')
+  find('.qa-Cl_order').click
 end
 
-Then(/^I fill the details to place the online order$/) do
+Then(/^I fill the details$/)do
+   sleep 10
 			  find('.Delivery').click
-        sleep 5
+       
             select('Business', :from => 'Address_Type_Select')
  		    fill_in('Street', :with => '7950 jones branch dr')
   			fill_in('City', :with => 'Mc Lean')
   			select('VA', :from => 'Region')
   			fill_in('Postal_Code', :with => '22102')
-  			find_button('Continue').click
-  			click_link('Popular Items')
-        require "debug"
-  			click_link('Large (14") Hand Tossed Pizza')
-  			click_link('Checkout')
-        find(:xpath, "//div[@class='js-beverageUpsell']/table[2]/tbody/tr/td/div/div[2]/div[2]/input").click
-        find(:css, '2LSPRITE').click
-  			#click_link('2LCOKE')
-  			click_link('Continue Checkout')
+      end
+
+Then(/^I click on the Continue button$/) do
+  find_button('Continue').click
+        sleep 5
+        click_link('Popular Items')
+        click_link('Large (14") Hand Tossed Pizza')
+        #find(:css,'#Future_Time').click
+        #select('11:00 am', :from => 'Future_Time')
+        #find(:css,'button.btn.btn--future-time').click
+        find(:css, 'a.qa-Coupon.grid > div.grid__cell--three-quarters.media__body > h2.media__title').click
+        find(:css, 'a.findCouponButton').click
+        click_link('Checkout')
+        #click_link('Continue Checkout')
+        #expect(page).to have_title('Entrees Home - Domino\'s Pizza, Order Pizza Online for Delivery - Dominos.com')
 end
+
+Then(/^I check the total on my order$/) do
+  Order_total=find(:xpath,'//div[1]/div[2]/div/div/div/div[2]/div/div[2]/div/table[3]/tbody/tr[3]/td[2]')
+  #Order_total=find(:xpath,'//div[1]/div[2]/div/div/div/div[2]/div/div[2]/div/table[3]/tbody/tr[4]/td[2]').value
+  print Order_total
+end
+
+Then(/^I add a coupon code$/) do
+  find(:css,'a.navigation-coupons').click
+ find(:xpath,"//div[@id='js-pageSplit']/section/div/div[4]/div[2]/div/a").Click
+ click_link('Checkout')
+        end
+
+Then(/^I add extras$/) do
+  find(:xpath,'//div[1]/div[2]/div/div/div/div[2]/section/div/div[3]/a').Click
+end
+
+Then(/^i check my total again$/) do
+  new_total=find(:xpath,'//div[1]/div[2]/div/div/div/div[2]/div/div[2]/div/table[4]/tbody/tr[4]/td[2]').native.text
+  expect(page).to.have('new_total')
+end
+        
